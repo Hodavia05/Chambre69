74b3d78825ae5c4902464821fb14a273c53994ba
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-04-03 17:13:24 +0100

    Add Postgresql database

---
+++ backend/sql/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
